22f61daa75fcbf8bdb2596cf290560443236ccf2
lemokjhb025 <lemokjhb025@student.wethinkcode.co.za>
2025-11-06 12:02:37 +0200

    Tried my best

--- main.py
+++ main.py
@@ -10,7 +10,15 @@ def sum_numbers(nums: list[int]) -> int:
     Example:
         sum_numbers([2, -3, 5, -1]) → (2 + 5) - (3 + 1) = 3
     """
-    pass
+    pos = 0
+    neg = 0
+    for num in nums:
+        if num >= 0:
+            pos += int(num)
+        elif num < 0:
+            neg += int(num)
+    
+    return pos + neg
 
 def is_anagram(str1: str, str2: str) -> bool:
     """
@@ -23,7 +31,13 @@ def is_anagram(str1: str, str2: str) -> bool:
         Input → str1 = "hello", str2 = "world"
         Output → False
     """
-    pass
+    str1 = str1.lower().replace(' ', '')
+    str2 = str2.lower().replace(' ', '')
+
+    if sorted(str1) == sorted(str2):
+        return True
+    else:
+        return False
 
 def fibonacci_series(n_terms: int) -> List[int]:
     """
@@ -35,7 +49,22 @@ def fibonacci_series(n_terms: int) -> List[int]:
         Input → 10
         Output → [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
     """
-    pass
+    n = [0, 1]
+    if n_terms <= 0:
+        return []
+    if n_terms == 1:
+        return [0]
+    if n_terms == 2:
+        return n
+    
+    for x in range(2, n_terms):
+        n1 = n[-2]
+        n2 = n[-1]
+        x = n1 + n2
+        n.append(x)
+    return n
+    
+    
 
 def prime_factors(n: int) -> List[int]:
     """
@@ -45,7 +74,13 @@ def prime_factors(n: int) -> List[int]:
         Input → 84
         Output → [2, 2, 3, 7]
     """
-    pass
+    # check if the number in the n range can go in n...
+    # if number can go through n check if it is a prime number...
+    # if number is a prime number append it to a list called factors...
+    # use floor division to get what is left of that...
+    # get thet to go through a loop...
+    # return factor list
+
 
 def create_pyramid(n):
     """
@@ -73,7 +108,7 @@ def create_multiplication_square(n: int) -> List[str]:
         Input → 3
         Output → ['1 2 3', '2 4 6', '3 6 9']
     """
-    pass
+
 
 
 def create_diamond(n):
@@ -92,4 +127,9 @@ def create_pascals_triangle(n: int) -> List[str]:
         Input → 5
         Output → ['    1    ', '   1 1   ',    '  1 2 1  ', ' 1 3 3 1 ', '1 4 6 4 1']
     """
+    pass
+
+
+
+if __name__ == '__main__':
     pass
